8f8d11409ae073ec6fc1f9afdfd80e130750cbc3
Simon K <jackofspaces@gmail.com>
2023-12-09 09:47:12 +0000

    app callbacks; add rich, add type-all extras

--- kafkapy/main.py
+++ kafkapy/main.py
@@ -1,11 +1,22 @@
 import typer
+import rich
 
+app = typer.Typer(
+    help="Python CLI for managing kafka clusters.",
+    rich_markup_mode="rich")
 
-def main(name: str):
-    """Entry point to kafkapy."""
-
+@app.command(rich_help_panel="Checks")
+def check():
+    """Execute various checks."""
     typer.Exit(code=0)
 
-
+@app.callback()
+def main(verbose: bool = False):
+    """
+    Entrypoint.
+    """
+    if verbose:
+        ...
+        
 if __name__ == "__main__":
-    typer.run(main)
+    app()
