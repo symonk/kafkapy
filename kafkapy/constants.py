47b88a7471e83b4b59f034210fbf28fd56119323
Simon K <jackofspaces@gmail.com>
2023-12-09 10:21:42 +0000

    starting on basic `docker-compose` cluster for testing purposes

---
+++ kafkapy/constants.py
@@ -0,0 +1,2 @@
+# Sub Commands
+CHECK_COMMAND_HELP = "[green][bold]CHECK COMMAND[/green][/bold]"
--- kafkapy/main.py
+++ kafkapy/main.py
@@ -1,22 +1,31 @@
 import typer
-import rich
+import time
+from rich.progress import track
+
 
 app = typer.Typer(
     help="Python CLI for managing kafka clusters.",
     rich_markup_mode="rich")
 
-@app.command(rich_help_panel="Checks")
+@app.command()
 def check():
     """Execute various checks."""
     typer.Exit(code=0)
 
+@app.command()
+def other():
+    """Placeholder."""
+
 @app.callback()
 def main(verbose: bool = False):
     """
-    Entrypoint.
+    A Suite of utilities for managing a [bold][red]kafka[/bold][/red] cluster.
     """
-    if verbose:
-        ...
-        
+    total = 0
+    for _ in track(range(10_000), description="[green][bold]initializing[green][bold]"):
+        time.sleep(0.01)
+        total += 1
+
+
 if __name__ == "__main__":
     app()
