5be34e31c85461ecf612e625a1113cc11160b935
Simon K <jackofspaces@gmail.com>
2023-12-29 10:24:44 +0000

    `integration` tests now spinning up kafka0 container

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 from kafkapy.main import app
 from typer.testing import CliRunner
+import pathlib
 import pytest
+from pytest_docker.plugin import Services
 test_runner = CliRunner()
 
 
@@ -16,3 +18,16 @@ def kafkapytester() -> CliRunner:
 def root_app():
     """Return the root (parent) app of kafkapy."""
     return app
+
+
+@pytest.fixture(scope="session")
+def docker_compose_file(pytestconfig: pytest.Config):
+    """Custom fixture for allowing the docker-compose.yml to reside outside
+     of the tests directory."""
+    return pathlib.Path(str(pytestconfig.rootdir)) / "docker-compose.yml"
+
+@pytest.fixture(scope="session")
+def kafka_service(docker_ip: str, docker_services: Services):
+    """Ensure that the kafka service is up and responsive."""
+    port = docker_services.port_for("kafka0", 9092)
+    # Todo: Add health check support for kafka services.
--- tests/test_topics.py
+++ tests/test_topics.py
@@ -1,10 +1,10 @@
 
 
-def test_bootstrap_servers_is_handled_correctly(root_app, kafkapytester) -> None:
+def test_bootstrap_servers_is_handled_correctly(root_app, kafkapytester, kafka_service) -> None:
     result = kafkapytester.invoke(root_app, ("topics", "list", "--bootstrap-servers", "localhost:1234", "--bootstrap-servers", "localhost:4321"))
     assert not result.exit_code 
 
-def test_invalid_bootstrap_servers(root_app, kafkapytester) -> None:
+def test_invalid_bootstrap_servers(root_app, kafkapytester, kafka_service) -> None:
     result = kafkapytester.invoke(root_app, ("topics", "list", "--bootstrap-servers", "local:host:1234"))
     assert result.exit_code == 2 
     assert "Invalid value for '--bootstrap-servers':" in result.stdout
