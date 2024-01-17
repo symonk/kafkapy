8e6ac40c050b3f5e58caeb6feb372ad862de47cb
Simon K <jackofspaces@gmail.com>
2024-01-17 21:46:28 +0000

    Test fixes

--- tests/io/test_root.py
+++ tests/io/test_root.py
@@ -26,11 +26,11 @@ def test_default_properties_dir_is_overwritable_from_environment() -> None:
 def test_bootstrap_servers_is_handled_correctly(root_app, kafkapytester) -> None:
     with pytest.raises(KafkaException, match=r".*Failed to get metadata: Local: Broker transport failure"):
         result = kafkapytester.invoke(root_app, ("topics", "list", "--bootstrap-servers", "localhost:1234", "--bootstrap-servers", "localhost:4321", "--timeout", "5.0"), catch_exceptions=False)
-        assert result.exit_code == 1
+        assert result.exit_code
         assert result.return_value is None
 
 @RequiresDockerKafka
 def test_invalid_bootstrap_servers(root_app, kafkapytester) -> None:
     result = kafkapytester.invoke(root_app, ("topics", "list", "--bootstrap-servers", "local:host:1234", "--timeout", "5.0"))
-    assert result.exit_code
+    assert result.exit_code 
     assert "Invalid value for '--bootstrap-servers':" in result.stdout
